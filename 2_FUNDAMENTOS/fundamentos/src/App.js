181b7ce5528d8ed5956c9942adb62f1a5bc39599
Guilherme <guilherme@zscansoftware.com>
2023-02-09 22:36:28 -0300

    Fundamentos React.

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+//style
+import './App.css';
+//components
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
